386d0b167c911dd8cd2d551fd2f82e485433a3db
jsgfhgn <129336487+teranga-ai@users.noreply.github.com>
2023-06-03 03:01:57 +0000

    Update index.php

--- Gestion des participants/index.php
+++ Gestion des participants/index.php
@@ -16,12 +16,12 @@ include "db_conn.php";
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-  <title>PHP CRUD Application</title>
+  <title>Gestion des Participants</title>
 </head>
 
 <body>
   <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: gray;">
-    Application sur la Gestion des Participation a la Formation Cloud AWS Cote d'Ivoire
+    Application sur la Gestion des Participation a la Formation Cloud AWS Simplon Cote d'Ivoire
   </nav>
 
   <div class="container">
